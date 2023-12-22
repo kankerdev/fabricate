c206b23fdfa96754ddd03b6acb023c4d661e85cc
Tyrone Faulhaber <20131658+spectrapulse@users.noreply.github.com>
2023-12-22 16:56:40 +0100

    v0.2.1 moment, made a few mods optional and added mouse tweaks back

---
+++ set_version.ps1
@@ -0,0 +1,22 @@
+# Update modpack version using regex
+$file = ".\pack.toml"
+$regex = 'version \= "\d*\.\d*\.\d*"'
+(Get-Content $file) -Replace $regex, ('version = "{0}"' -f $args[0]) | Set-Content $file
+
+# Update version in titlebar
+$file = ".\config\barista\bar.json"
+$regex = '"context": "Fabricate v\d*\.\d*\.\d*"'
+(Get-Content $file) -Replace $regex, ('"context": "Fabricate v{0}"' -f $args[0]) | Set-Content $file
+
+# Update version in main menu credits
+$file = ".\config\isxander-main-menu-credits.json"
+$regex = '"text": "Fabricate v\d*\.\d*\.\d*"'
+(Get-Content $file) -Replace $regex, ('"text": "Fabricate v{0}"' -f $args[0]) | Set-Content $file
+
+# Update pack compat checker versioning
+$file = ".\config\bcc.json"
+$regex = '"modpackVersion": "v\d*\.\d*\.\d*",'
+(Get-Content $file) -Replace $regex, ('"text": "Fabricate v{0}",' -f $args[0]) | Set-Content $file
+
+# Refresh packwiz index
+packwiz refresh
