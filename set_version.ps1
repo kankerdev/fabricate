1133e401aedb8233ad3af04552441b6bf824cef3
Tyrone Faulhaber <20131658+spectrapulse@users.noreply.github.com>
2023-12-23 15:51:48 +0100

    Set pack version to 0.2.3b and updated the version script to support the channel

--- set_version.ps1
+++ set_version.ps1
@@ -1,21 +1,21 @@
 # Update modpack version using regex
 $file = ".\pack.toml"
-$regex = 'version \= "\d*\.\d*\.\d*"'
+$regex = 'version \= "\d*\.\d*\.\d*\w?"'
 (Get-Content $file) -Replace $regex, ('version = "{0}"' -f $args[0]) | Set-Content $file
 
 # Update version in titlebar
 $file = ".\config\barista\bar.json"
-$regex = '"context": "Fabricate v\d*\.\d*\.\d*"'
+$regex = '"context": "Fabricate v\d*\.\d*\.\d*\w?"'
 (Get-Content $file) -Replace $regex, ('"context": "Fabricate v{0}"' -f $args[0]) | Set-Content $file
 
 # Update version in main menu credits
 $file = ".\config\isxander-main-menu-credits.json"
-$regex = '"text": "Fabricate v\d*\.\d*\.\d*"'
+$regex = '"text": "Fabricate v\d*\.\d*\.\d*\w?"'
 (Get-Content $file) -Replace $regex, ('"text": "Fabricate v{0}"' -f $args[0]) | Set-Content $file
 
 # Update pack compat checker versioning
 $file = ".\config\bcc.json"
-$regex = '"modpackVersion": "v\d*\.\d*\.\d*",'
+$regex = '"modpackVersion": "v\d*\.\d*\.\d*\w?",'
 (Get-Content $file) -Replace $regex, ('"modpackVersion": "v{0}",' -f $args[0]) | Set-Content $file
 
 # Refresh packwiz index
